a5e41b13cd7ba100ca8b21e0212c6db19de2446a
ishanshams <ishanshams303@gmail.com>
2015-11-17 17:05:30 -0500

    ClassActivity
    Class Activity IT 306 11/17/2015

---
+++ src/Pair.java
@@ -0,0 +1,14 @@
+
+public class Pair<T,S> {
+	T first; 
+	S second; 
+	
+	public Pair(T u1 , S u2){
+		first = u1;
+		second = u2; 
+	}
+	
+	public T getFirst() { return first; }
+	public S getSecond() { return second; }
+	
+}
